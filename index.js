657fe9ed2c9f6d962c96662f6df88c0c06a98f4b
MinGyu, Ju <knighp@gmail.com>
2021-03-29 19:50:08 +0900

    2

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const http = require('http')
+const history = require('connect-history-api-fallback')
 
-app.use(express.static('./dist'))
-
-app.get('/', (req, res) => {
-  res.send('index.html');
+const app = express()
+const server = http.createServer(app)
+const port = process.env.PORT || 1337
+const options = {
+  index: 'index.html'
+}
+var middleware = history({
+  index: '/index.html'
 })
 
-app.listen(8000,() => {
-  console.log(`server open: ${8000}`)
+app.use(middleware)
+app.use('/', express.static('/home/site/wwwroot', options))
+
+server.listen(port, function() {
+  console.log('Server listen on port!' + server.address().port)
 })
